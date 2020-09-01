51742fcee126b88142fbfafb2a7718e57f958f19
Max Miller <mpm1900@users.noreply.github.com>
2020-09-01 12:52:59 -0500

    Updates

---
+++ precache-manifest.8965d584cda4a79bd657a3a1b540aec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ff36e9f8d8bb311df49ab8f85bad9a",
+    "url": "/jsftk/index.html"
+  },
+  {
+    "revision": "c3d24ce7c8a6773d3542",
+    "url": "/jsftk/static/css/main.b4ab37af.chunk.css"
+  },
+  {
+    "revision": "818347c53b28e4740046",
+    "url": "/jsftk/static/js/2.9bddd80f.chunk.js"
+  },
+  {
+    "revision": "4fc96d8633d093f6effd8ad2d7e6794c",
+    "url": "/jsftk/static/js/2.9bddd80f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d24ce7c8a6773d3542",
+    "url": "/jsftk/static/js/main.f6e95f0a.chunk.js"
+  },
+  {
+    "revision": "01e26c671c51f8b1bf8b",
+    "url": "/jsftk/static/js/runtime-main.beaa66dd.js"
+  }
+]);
