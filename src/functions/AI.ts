b9015bc39726f15f86ca7fd637f250b223231508
Max Miller <mpm1900@users.noreply.github.com>
2020-09-04 22:16:34 -0500

    don't let enemies target dead pcs

--- src/functions/AI.ts
+++ src/functions/AI.ts
@@ -12,14 +12,14 @@ export const getAIAction = (
   enemyParty: ProcessedPartyT,
 ): AIActionT => {
   const skill = getRandom(source.skills)
-  const targetedCharacters = playerParty.characters.filter((c) =>
-    c.status.map((t) => t.type).includes('targeted'),
+  const targetedCharacters = playerParty.characters.filter(
+    (c) => c.status.map((t) => t.type).includes('targeted') && !c.dead,
   )
   const target =
     skill.targetType === 'single'
       ? targetedCharacters.length > 0
         ? getRandom(targetedCharacters)
-        : getRandom(playerParty.characters)
+        : getRandom(playerParty.characters.filter((c) => !c.dead))
       : getRandom<ProcessedPartyT | ProcessedCharacterT>(
           getSkillTargetOptions(source, enemyParty, playerParty, skill),
         )
